398e2913fb40d568225536a6c5941fd6caeebbd5
qawizard <qawizard@yandex-team.ru>
2017-07-19 19:07:00 +0300

    Removed outdated code

--- conftest.py
+++ conftest.py
@@ -9,19 +9,9 @@ def get_test_case_docstring(item):
     param_str = 'long string ({0} characters) started from "{1}"'
 
     if item._obj.__doc__:
-
-        # Determine the name of component (Note: the name of directory
-        # inside the root of test repository should be equal to the name
-        # of the component):
-        previous = ''
-        for name in str(item.fspath).split('/'):
-            if previous == 'component_tests':
-                full_name = name.upper() + ' | '
-            previous = name
-
         # Remove extra whitespaces from the doc string:
         name = item._obj.__doc__.strip()
-        full_name += ' '.join(name.split())
+        full_name = ' '.join(name.split())
 
         # Generate the list of parameters for parametrized test cases:
         if hasattr(item, 'callspec'):
@@ -69,3 +59,4 @@ def pytest_collection_finish(session):
                 print full_name
 
     pytest.exit('Done!')
+
